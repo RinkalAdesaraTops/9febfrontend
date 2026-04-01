1bcfa927f432c1c2cab3124002f7e40b78ea7b13
Rinkal <rinkaladesara.tops@gmail.com>
2026-04-01 09:23:49 +0530

    js

---
+++ JS/second.js
@@ -0,0 +1,22 @@
+a=10
+b=20
+c=a+b
+document.write('Addition is ',c)
+document.write('<br>Minus is '+(a-b))
+//var const let
+// var = redeclare & reassign is possible
+var x=100
+document.write('<br>x is '+x)
+var x="Hello"
+document.write('<br>x is '+x)
+//const  = cant reassign & redeclare 
+const pi=3.14
+document.write('<br>PI is '+pi)
+// const pi=956879
+// document.write('<br>PI is '+pi)
+// let - cant redeclare but value can be changed
+let name ="test"
+document.write('<br>Name is '+name)
+name="abc123"
+document.write('<br>Name is '+name)
+
